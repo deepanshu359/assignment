a7eecac9fde45f2f2b5b68d17eaffeaf62d640f9
deepanshu359 <vermadeepanshu123@gmail.com>
2018-09-28 03:00:46 -0700

    Second assignment

---
+++ Assignment2.py
@@ -0,0 +1,41 @@
+ASSIGNMENT- PRINT STATEMENT AND BASIC MATH
+
+#Q.1- Print anything you want on screen.
+ print("My name is Deepanshu Verma(150359) B.Tech CSE &th sem student of Shoolini University")
+
+#Q.2- Join two strings using '+'. 
+#E.g.-"Acad"+"View”
+ c= "Chota"+"Don"
+print(c)
+
+Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+      x=input("Enter the value of x")
+      y=input("Enter the value of y")
+      z=input("Enter the value of z")
+print(x,y,z)
+
+#Q.4- Print “Let’s get started” on screen.
+print('Let\'s get started')
+
+
+#Q.5- Print the following values using placeholders. 
+#s=”Acadview” 
+#course=”Python” 
+#fees=5000
+     s="Acadview"
+     print(s)
+     course="Python"
+     print(course)
+     fees=5000
+     print(fees)
+
+
+#Q.6- Find the area of circle 
+#pi = 3.14 
+#Take radius as input from user 
+#Print the area of circle .
+ pi=3.14
+ r=float(input("Enter the radius:"))
+ print(pi*r*r)
+ 
+     
