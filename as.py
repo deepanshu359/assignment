4f4c69cf44d8af22ae4af4a2984c01a82df5db86
deepanshu359 <43607153+deepanshu359@users.noreply.github.com>
2018-09-28 06:12:48 -0700

    Delete as.py

--- as.py
+++
@@ -1,7 +0,0 @@
-num1=int(input("Enter a no"))
-num2=int(input("Enter a no"))
-res=num1+num2
-#print(num1,'+',num2,'=',num1+num2)
-print (num1,'+',num2,'=',res)
-print(str(num1)+ '+' +str(num2)+'=' +str(res))
-
